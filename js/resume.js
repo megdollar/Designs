f69913c411cf362bc7e32640c90c4309c48dd056
Megan Dollar <megdollar@gmail.com>
2017-03-07 15:14:33 -0500

    Add files via upload

--- js/resume.js
+++ js/resume.js
@@ -3,3 +3,9 @@
  for(i = 0; i < listItem.length; i++){
  	listItem[i].style.display = "list-item";
  };
+
+var $iW = $(window).innerWidth();
+
+    if ($iW <= 767) {
+        $('.navAbout').insertBefore('.navMenu');
+    } 
