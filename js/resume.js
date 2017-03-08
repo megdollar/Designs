c8a2989b74e2019b3914022840508b99e64e130c
Megan Dollar <megdollar@gmail.com>
2017-03-07 19:36:14 -0500

    Add files via upload

--- js/resume.js
+++ js/resume.js
@@ -9,3 +9,12 @@ var $iW = $(window).innerWidth();
     if ($iW <= 767) {
         $('.navAbout').insertBefore('.navMenu');
     } 
+    
+
+  $(window).resize(function(){
+	if ($(window).width() <= 767){	
+		$('.navAbout').insertBefore('.navMenu');
+	}	else {
+		$('.navAbout').insertAfter('.main');
+	}
+});
